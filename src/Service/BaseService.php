bd904a75d2ecf49adc42558e1f54eaa53b883e56
Бельмесова Марина <bem@jcat.ru>
2022-09-16 15:22:07 +0300

    Перетаскиваемые стикеры. TODO: сделать перетаскивание окон. Стикеры положить на фон?

--- src/Service/BaseService.php
+++ src/Service/BaseService.php
@@ -4,22 +4,31 @@ namespace App\Service;
 
 class BaseService
 {
+    public static function numberDeclination($n, $titles)
+    {
+        $cases = array(2, 0, 1, 1, 1, 2);
+
+        return $titles[($n % 100 > 4 && $n % 100 < 20) ? 2 : $cases[min($n % 10, 5)]];
+    }
+
     public function getResume()
     {
         $howOldAmINow = (new \DateTime())->diff(new \DateTime('1998-12-23'))->y;
+        $howOldAmINow .= ' ' . self::numberDeclination($howOldAmINow, ['год', 'года', 'лет']);
         /* Скопировано с резюме на хх
         TODO: расписать поинтереснее */
         $array = [
-            'Кто я' => 'Женщина, ' . $howOldAmINow . ' года, родилась 23 декабря 1998. Вредных привычек не имею.',
-            'Что я могу' => 'Быстро обучаюсь новому, честно и ответственно подхожу к работе. ',
-            'Мой опыт' => 'Занимаюсь back-end-разработками в компании Джейкет.ру. ',
+            ['header' => 'Кто я',
+                'text' => 'Женщина, ' . $howOldAmINow . ', родилась 23 декабря 1998. Вредных привычек не имею.',
+                'sticker' => 'flow1.png'],
+            ['header' => 'Что я могу',
+                'text' => 'Быстро обучаюсь новому, честно и ответственно подхожу к работе. ',
+                'sticker' => 'flow2.png'],
+            ['header' => 'Мой опыт',
+                'text' => 'Занимаюсь back-end-разработками в компании Джейкет.ру. ',
+                'sticker' => 'flow3.png'],
         ];
 
-        return array_map(function ($header, $text) {
-            return array(
-                'header' => $header,
-                'text' => $text,
-            );
-        }, array_keys($array), $array);
+        return $array;
     }
 }
